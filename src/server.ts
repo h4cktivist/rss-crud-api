a6809a8ad8bc4d3942497b5175c357db4a87e3e5
h4cktivist <popovvadim0605@gmail.com>
2024-02-05 09:43:55 +0500

    feat: error handling add

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,29 @@ import {getUsers, addUser, updateUser, deleteUser, sendResponse} from './utils';
 const CONTENT_TYPE: http.OutgoingHttpHeaders = { 'Content-Type': 'application/json' };
 export const server: http.Server = http.createServer( (req: http.IncomingMessage, res: http.ServerResponse): void => {
     if (req.method === 'GET') {
-        getUsers(res, req.url);
+        try {
+            getUsers(res, req.url);
+        } catch {
+            sendResponse(res, 500, CONTENT_TYPE, {error: 'Internal server error'});
+        }
     } else if (req.method === 'POST' && req.url === '/api/users') {
-        addUser(req, res);
+        try {
+            addUser(req, res);
+        } catch {
+            sendResponse(res, 500, CONTENT_TYPE, {error: 'Internal server error'});
+        }
     } else if (req.method === 'PUT' && req.url?.startsWith('/api/users')) {
-        updateUser(req, res, req.url);
+        try {
+            updateUser(req, res, req.url);
+        } catch {
+            sendResponse(res, 500, CONTENT_TYPE, {error: 'Internal server error'});
+        }
     } else if (req.method === 'DELETE' && req.url?.startsWith('/api/users')) {
-        deleteUser(res, req.url);
+        try {
+            deleteUser(res, req.url);
+        } catch {
+            sendResponse(res, 500, CONTENT_TYPE, {error: 'Internal server error'});
+        }
     } else {
         sendResponse(res, 404, CONTENT_TYPE, {error: 'Method is not allowed or URL is not found!'});
     }
