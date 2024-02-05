7d7791cb9558aa7ac8fa74e1af72d8a069526e2d
h4cktivist <popovvadim0605@gmail.com>
2024-02-05 09:40:17 +0500

    feat: user delete add

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import http from 'http';
-import {getUsers, addUser, updateUser, sendResponse} from './utils';
+import {getUsers, addUser, updateUser, deleteUser, sendResponse} from './utils';
 
 const CONTENT_TYPE: http.OutgoingHttpHeaders = { 'Content-Type': 'application/json' };
 export const server: http.Server = http.createServer( (req: http.IncomingMessage, res: http.ServerResponse): void => {
     if (req.method === 'GET') {
         getUsers(res, req.url);
-    } else if (req.method == 'POST' && req.url === '/api/users') {
+    } else if (req.method === 'POST' && req.url === '/api/users') {
         addUser(req, res);
-    } else if (req.method == 'PUT' && req.url?.startsWith('/api/users')) {
+    } else if (req.method === 'PUT' && req.url?.startsWith('/api/users')) {
         updateUser(req, res, req.url);
+    } else if (req.method === 'DELETE' && req.url?.startsWith('/api/users')) {
+        deleteUser(res, req.url);
     } else {
-        sendResponse(res, 404, CONTENT_TYPE, {error: 'Method is not allowed!'});
+        sendResponse(res, 404, CONTENT_TYPE, {error: 'Method is not allowed or URL is not found!'});
     }
 });
--- src/utils.ts
+++ src/utils.ts
@@ -80,3 +80,18 @@ export const updateUser = (req: http.IncomingMessage, res: http.ServerResponse, 
         }
     });
 };
+
+export const deleteUser = (res: http.ServerResponse, url: string): void => {
+    const id: string = url?.split('/').slice(-1)[0];
+    if (validate(id)) {
+        const userIndex: number = users.findIndex(u => u.id === id);
+        if (userIndex !== -1) {
+            users.splice(userIndex, 1);
+            sendResponse(res, 204, CONTENT_TYPE, {});
+        } else {
+            sendResponse(res, 404, CONTENT_TYPE, { error: 'User is not found' });
+        }
+    } else {
+        sendResponse(res, 400, CONTENT_TYPE, { error: 'User ID is incorrect' });
+    }
+};
